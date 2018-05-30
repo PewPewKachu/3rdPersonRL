0258965d27bc91843128c46eb32839a1d8481ff0
Jacob Presley <jacobpresley32@gmail.com>
2018-05-30 16:35:04 -0400

    added simple raycasting

--- 3rdPersonRL/Assets/CameraScript.cs
+++ 3rdPersonRL/Assets/CameraScript.cs
@@ -7,17 +7,18 @@ public class CameraScript : MonoBehaviour
     private const float Y_ANGLE__MAX = 80.0f;
 
     public Transform lookAt;
-    public Transform camTransform;
 
     private Camera cam;
 
     private float distance = 10.0f;
     [SerializeField]
-    private float distY = 1.0f;
+    private float distY = 1.5f;
     private float currentX = 0.0f;
     private float currentY = 0.0f;
     private float sensX = 2.0f;
     private float sensY = 2.0f;
+
+    bool isBlocked = false;
 
     // Use this for initialization
     void Start()
@@ -29,15 +30,46 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(lookAt.transform.position, hit.point, Color.red);
+            
+        }
+
+        #region MouseLookInput
         currentX += Input.GetAxis("Mouse X") * sensX;
         currentY -= Input.GetAxis("Mouse Y") * sensY;
-
         currentY = Mathf.Clamp(currentY, Y_ANGLE__MIN, Y_ANGLE__MAX);
+        #endregion
 
         Vector3 dir = new Vector3(0, 0, -distance);
         Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
         transform.position = lookAt.position + rotation * dir;
         transform.LookAt(new Vector3(lookAt.position.x, lookAt.position.y + distY, lookAt.position.z));
+
+        if (!isBlocked)
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                distance -= 1.0f;
+                if (distance < 7.5f)
+                {
+                    distance = 7.5f;
+                }
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                distance += 1.0f;
+                if (distance > 15.0f)
+                {
+                    distance = 15.0f;
+                }
+            }
+        }
+
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.blue);
     }
 
     void LateUpdate()
