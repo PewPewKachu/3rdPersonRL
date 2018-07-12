a45c424cd957e7f3adfba922c1f65ccb9e62a40e
Jacob Presley <jacobpresley32@gmail.com>
2018-07-12 19:40:02 -0400

    First Jump Mechanics

--- 3rdPersonRL/Assets/playerController.cs
+++ 3rdPersonRL/Assets/playerController.cs
@@ -6,10 +6,20 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float speed = 15;
     CharacterController myCC;
+    float yVel;
+    [SerializeField]
+    float timeInAir;
+    [SerializeField]
+    int numJumps;
+    [SerializeField]
+    bool isGrounded;
     // Use this for initialization
     void Start()
     {
         myCC = GetComponent<CharacterController>();
+        yVel = -1;
+        timeInAir = 0;
+        numJumps = 2;
     }
 
     // Update is called once per frame
@@ -18,9 +28,38 @@ public class PlayerController : MonoBehaviour
         float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
 
+        isGrounded = myCC.isGrounded;
+
+        if (myCC.isGrounded)
+        {
+            yVel = -1;
+            timeInAir = 0;
+            numJumps = 2;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                yVel = 15f;
+                numJumps -= 1;
+            }
+
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && numJumps > 0)
+            {
+                yVel = 15f;
+                numJumps -= 1;
+                timeInAir = 0;
+            }
+            timeInAir += Time.fixedDeltaTime * 3f;
+        }
+
+        yVel -= 25f * timeInAir * Time.fixedDeltaTime;
+
 
         myCC.Move(transform.forward * speed * vAxis * Time.deltaTime);
         myCC.Move(transform.right * speed * hAxis * Time.deltaTime);
+        myCC.Move(transform.up * yVel * Time.deltaTime);
 
     }
 }
